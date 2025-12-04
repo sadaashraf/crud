b803e53d7acf3929ff39e89b27601881f881a3f3
Muhammad Ashraf <nayeemashraf92@gmail.com>
2025-12-04 15:47:12 +0500

    api working

--- src/crud/crud.service.ts
+++ src/crud/crud.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCrudDto } from './dto/create-crud.dto';
 import { UpdateCrudDto } from './dto/update-crud.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -17,18 +17,36 @@ export class CrudService {
   }
 
   findAll() {
-    return `This action returns all crud`;
+    const crud = this.crudRepository.find()
+    return crud;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} crud`;
+    const crud = this.crudRepository.findOneBy({ id })
+    return crud
+
   }
 
-  update(id: number, updateCrudDto: UpdateCrudDto) {
-    return `This action updates a #${id} crud`;
+  async update(id: number, updateCrudDto: UpdateCrudDto) {
+    const crud = await this.crudRepository.findOneBy({ id })
+    if (!crud) {
+      return new NotFoundException('id not found')
+    }
+    Object.assign(crud, updateCrudDto)
+    return this.crudRepository.save(crud)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} crud`;
+  async remove(id: number) {
+    const crud = await this.crudRepository.findOneBy({ id })
+    if (!crud) {
+      return new NotFoundException('id not found')
+
+    }
+    await this.crudRepository.remove(crud)
+    return {
+      message: "deleted successfully"
+    }
+
+    // console.log('remove object', crud)
   }
 }
